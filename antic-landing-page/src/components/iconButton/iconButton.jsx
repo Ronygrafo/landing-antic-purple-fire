8c4240356fac5f6b920a9bdd64d4082e8724adb8
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2024-10-20 12:54:32 -0500

    IconButton Component with BEM clases Done!

--- antic-landing-page/src/components/iconButton/iconButton.jsx
+++ antic-landing-page/src/components/iconButton/iconButton.jsx
@@ -1,12 +1,43 @@
 import { MaterialSymbol } from 'react-material-symbols';
 import './IconButton.css'
 
-const IconButton = ({iconName, size, fill, grade, color }) => {
-  return (
-    <button>
-      <MaterialSymbol icon={iconName} size={size} fill={fill} grade={grade} color={color} />
-    </button>
-  )
-}
+const IconButton = ({ 
+  iconName, 
+  variant = '', 
+  size = '', 
+  iconFill = 'false',
+  onClick 
+}) => {
+  const getIconColor = () => {
+    return variant === 'primary' ? 'var(--color-primary)' : 'var(--color-white)';
+  };
 
-export default IconButton
+  const getIconSize = () => {
+    switch(size) {
+      case 'small': return 20;
+      case 'large': return 32;
+      default: return 24;
+    }
+  };
+
+  const materialSymbolProps = {
+    icon: iconName,
+    size: getIconSize(),
+    color: getIconColor()
+  };
+
+  if (iconFill === 'true') {
+    materialSymbolProps.iconFill = true;
+  }
+
+  return (
+    <button 
+      className={`button button--${variant} button--${size}`}
+      onClick={onClick}
+    >
+      <MaterialSymbol {...materialSymbolProps} />
+    </button>
+  );
+};
+
+export default IconButton;
--- antic-landing-page/src/pages/LandingPage/LandingPage.jsx
+++ antic-landing-page/src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,44 @@
-import IconButton from '../../components/iconButton/iconButton'
-import './LandingPage.css'
+import IconButton from "../../components/iconButton/iconButton";
+import "./LandingPage.css";
 
 const LandingPage = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("Btn Clicked");
+  };
+
   return (
     <>
-    PAGE
-    <IconButton/>
+      <IconButton
+        iconName="keyboard_arrow_down"
+        variant="primary"
+        size="base"
+        iconFill="false"
+        onClick={handleClick}
+      />
+      <IconButton
+        iconName="forum"
+        variant="secondary"
+        size="base"
+        iconFill="true"
+        onClick={handleClick}
+      />
+            <IconButton
+        iconName="chevron_left"
+        variant="primary"
+        size="small"
+        iconFill="false"
+        onClick={handleClick}
+      />
+      <IconButton
+        iconName="chevron_right"
+        variant="primary"
+        size="small"
+        iconFill="false"
+        onClick={handleClick}
+      />
     </>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
