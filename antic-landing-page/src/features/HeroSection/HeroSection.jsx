8614e4bd39f832da65c873b8b38c4b9b515ee347
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2024-10-21 16:20:36 -0500

    Mobile Responsive behavior Done!

--- antic-landing-page/src/features/HeroSection/HeroSection.jsx
+++ antic-landing-page/src/features/HeroSection/HeroSection.jsx
@@ -10,23 +10,25 @@ const HeroSection = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-        <h2 className="varta-bold">HOME DESIGN</h2>
-        <h1 className="merriweather-light">
-          Elegance for Interiors & Exteriors
-        </h1>
-        <p className="varta-regular">
-          We provide everyone with modern, trendy, quality furniture
-        </p>
-        <IconButton
-          iconName="keyboard_arrow_down"
-          variant="primary"
-          size="base"
-          iconFill="false"
-          onClick={handleClick}
-        />
+        <div className="hero-text-container">
+          <h2 className="varta-bold">HOME DESIGN</h2>
+          <h1 className="merriweather-light">
+            Elegance for Interiors & Exteriors
+          </h1>
+          <p className="varta-regular">
+            We provide everyone with modern, trendy, quality furniture
+          </p>
+          <IconButton
+            iconName="keyboard_arrow_down"
+            variant="primary"
+            size="base"
+            iconFill="false"
+            onClick={handleClick}
+          />
+        </div>
       </div>
       <div className="hero-image">
-        <img src="../../../lamps.jpeg" alt="Beautiful Lamps" loading="lazy"/>
+        <img src="../../../lamps.jpeg" alt="Beautiful Lamps" loading="lazy" />
       </div>
     </section>
   );
