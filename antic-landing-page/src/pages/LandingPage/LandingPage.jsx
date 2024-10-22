593f865ca4135484ff933e7c09fd710246399c85
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2024-10-21 22:23:43 -0500

    Vercel Deployment hotfix

--- antic-landing-page/src/pages/LandingPage/LandingPage.jsx
+++ antic-landing-page/src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,3 @@
-//import IconButton from "../../components/iconButton/iconButton";
 import ImageCarousel from "../../features/Carousel/ImageCarousel";
 import FloatingButton from "../../features/FloatingButton/FloatingButton";
 import Footer from "../../features/Footer/Footer";
@@ -6,7 +5,6 @@ import HeroSection from "../../features/HeroSection/HeroSection";
 import MenuBar from "../../features/MenuBar/MenuBar";
 import { MulticolumnSection } from "../../features/MulticolumnSection/MulticolumnSection";
 import SuscriptionCTA from "../../features/SuscriptionCTA/SuscriptionCTA";
-import "./LandingPage.css";
 
 const LandingPage = () => {
   /*   const handleClick = (e) => {
